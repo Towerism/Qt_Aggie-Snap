8434350c21acc73cbae6352a425563d9ceb2e0d0
Martin <martin.frackerjr@gmail.com>
2013-12-03 01:01:44 -0600

    Stub menubar functionality added

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,14 +1,24 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
-MainWindow::MainWindow(QWidget *parent) :
-    QMainWindow(parent),
-    ui(new Ui::MainWindow)
-{
+MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent), ui(new Ui::MainWindow) {
     ui->setupUi(this);
+    createMenuFunctionality();
 }
 
-MainWindow::~MainWindow()
-{
+MainWindow::~MainWindow() {
     delete ui;
+}
+
+void MainWindow::createMenuFunctionality() {
+
+    connect(ui->action_Open_Image, SIGNAL(triggered()), this, SLOT(showNotImplemented()));
+    connect(ui->action_Download_Image, SIGNAL(triggered()), SLOT(showNotImplemented()));
+    connect(ui->action_Quit, SIGNAL(triggered()), this, SLOT(close()));
+    connect(ui->action_Filter_Images, SIGNAL(triggered()), SLOT(showNotImplemented()));
+    connect(ui->action_Delete_Image, SIGNAL(triggered()), this, SLOT(showNotImplemented()));
+    connect(ui->action_About_AgSnap, SIGNAL(triggered()), SLOT(showNotImplemented()));
+}
+
+void MainWindow::showNotImplemented() {
 }
--- mainwindow.h
+++ mainwindow.h
@@ -15,8 +15,12 @@ public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
 
+private slots:
+    void showNotImplemented();
+
 private:
     Ui::MainWindow *ui;
+    void createMenuFunctionality();
 };
 
 #endif // MAINWINDOW_H
